7ceb566ac5ebc056682152c566d8a761d8483fc2
nick-gorman <40549624+nick-gorman@users.noreply.github.com>
2020-05-11 21:54:17 +1000

    inputs manager 4

--- nempy/historical_inputs.py
+++ nempy/historical_inputs.py
@@ -57,11 +57,21 @@ def download_to_df(url, table_name, year, month):
     -------
     pd.DataFrame
 
+    Raises
+    ------
+    MissingData
+        If internet connection is down, nemweb is down or data requested is not on nemweb.
+
     """
     # Insert the table_name, year and month into the url.
     url = url.format(table=table_name, year=year, month=str(month).zfill(2))
     # Download the file.
     r = requests.get(url)
+    if r.status_code != 200:
+        raise MissingData(('Requested data for table: {}, year: {}, month: {} not downloaded.' +
+                           '\nPlease check your internet connection. Also check' 
+                           '\nhttp://nemweb.com.au/#mms-data-model, to see if your requested' +
+                           '\ndata is uploaded.').format(table_name, year, month))
     # Convert the contents of the response into a zipfile object.
     zf = zipfile.ZipFile(io.BytesIO(r.content))
     # Get the name of the file inside the zip object, assuming only one file is zipped inside.
@@ -71,33 +81,110 @@ def download_to_df(url, table_name, year, month):
     return data
 
 
+class MissingData(Exception):
+    """Raise for nemweb not returning status 200 for file request."""
+
+
 class MMSTable:
+    """Manages Market Management System (MMS) tables stored in an sqlite database.
+
+    This class creates the table in the data base when the object is instantiated. Methods for adding adding and
+    retrieving data are added by sub classing.
+    """
     def __init__(self, table_name, table_columns, table_primary_keys, con):
+        """Creates a table in sqlite database that the connection is provided for.
+
+        Examples
+        --------
+        This class is designed to be used after subclassing, however this is how it would be used on it own.
+
+        >>> connection = sqlite3.connect('the_database.db')
+
+        >>> table = MMSTable(table_name='a_table', table_columns=['col_1', 'col_2'], table_primary_keys=['col_1'],
+        ...                  con=connection)
+
+        Parameters
+        ----------
+        table_name : str
+            Name of the table.
+        table_columns : list(str)
+            List of table column names.
+        table_primary_keys : list(str)
+            Table columns to use as primary keys.
+        con : sqlite3.Connection
+            Connection to an existing database.
+        """
         self.table_name = table_name
         self.table_columns = table_columns
         self.table_primary_keys = table_primary_keys
-        self.url = 'http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/{year}/MMSDM_{year}_{month}/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_{table}_{year}{month}010000.zip'
+        # url that sub classes will use to pull MMS tables from nemweb.
+        self.url = 'http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/{year}/MMSDM_{year}_{month}/' + \
+                   'MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_{table}_{year}{month}010000.zip'
         with con:
             cur = con.cursor()
             cur.execute("""DROP TABLE IF EXISTS {};""".format(table_name))
             base_create_query = """CREATE TABLE {}({}, PRIMARY KEY ({}));"""
-            columns = ','.join(['{} TEXT'.format(col) for col in self.table_columns[table_name]])
-            primary_keys = ','.join(['{}'.format(col) for col in self.table_primary_keys[table_name]])
+            columns = ','.join(['{} TEXT'.format(col) for col in self.table_columns])
+            primary_keys = ','.join(['{}'.format(col) for col in self.table_primary_keys])
             create_query = base_create_query.format(table_name, columns, primary_keys)
             cur.execute(create_query)
             con.commit()
 
 
 class SingleDataSource(MMSTable):
-    def __init__(self, table_name, table_columns, table_primary_keys):
-        MMSTable.__init__(self, table_name, table_columns, table_primary_keys)
+    """Manages downloading data from nemweb for tables where all relevant data is stored in lasted data file."""
+    def __init__(self, table_name, table_columns, table_primary_keys, con):
+        MMSTable.__init__(self, table_name, table_columns, table_primary_keys, con)
 
-    @check.table_exists()
-    def add_data(self, table_name, year, month, con):
-        data = download_to_df(self.url, table_name, year, month)
-        data = data.loc[:, self.table_columns[table_name]]
+    def set_data(self, year, month, con):
+        """"Download data for the given table and time, replace any existing data.
+
+        Examples
+        --------
+        This class is designed to be used after subclassing, however this is how it would be used on it own. This
+        example will only work with an internet connection.
+
+        >>> connection = sqlite3.connect('the_database.db')
+
+        >>> table = SingleDataSource(table_name='DUDETAILSUMMARY',
+        ...                          table_columns=['DUID', 'START_DATE', 'CONNECTIONPOINTID', 'REGIONID'],
+        ...                          table_primary_keys=['START_DATE', 'DUID'], con=connection)
+
+        >>> table.set_data(year=2020, month=1, con=connection)
+
+        Now the database should contain data for this table that is up to date as the end of Janurary.
+
+        >>> query = "Select * from DUDETAILSUMMARY order by START_DATE DESC limit 1;"
+
+        >>> print(pd.read_sql_query(query, con=connection))
+                SETTLEMENTDATE REGIONID  TOTALDEMAND  DEMANDFORECAST  INITIALSUPPLY
+        0  2020/01/02 00:00:00     NSW1       6962.0       -34.85254      7021.4668
+
+        However if we subsequently set data from a previous date then any existing data will be replaced
+
+        >>> table.set_data(year=2020, month=1, con=connection)
+
+        >>> print(pd.read_sql_query(query, con=connection))
+        SETTLEMENTDATE REGIONID  TOTALDEMAND  DEMANDFORECAST  INITIALSUPPLY
+        0  2020/01/02 00:00:00     NSW1       6962.0       -34.85254      7021.4668
+
+        Parameters
+        ----------
+        year : int
+            The year to download data for.
+        month : int
+            The month to download data for.
+        con : sqlite3.Connection
+            Connection to a database in which the given table already exists.
+
+        Return
+        ------
+        None
+        """
+        data = download_to_df(self.url, self.table_name, year, month)
+        data = data.loc[:, self.table_columns]
         with con:
-            data.to_sql(table_name, con=con, if_exists='replace', index=False)
+            data.to_sql(self.table_name, con=con, if_exists='replace', index=False)
             con.commit()
 
 
@@ -176,7 +263,7 @@ class DBManager:
                                                        'T2', 'T3', 'T4'],
             table_primary_keys=['SETTLEMENTDATE', 'DUID', 'BIDTYPE'])
         self.DISPATCHREGIONSUM = InputsBySettlementDate(
-            table_name='DISPATCHREGIONSUM', table_columns=['SETTLEMENTDATE', 'DUID', 'TOTALDEMAND',
+            table_name='DISPATCHREGIONSUM', table_columns=['SETTLEMENTDATE', 'REGIONID', 'TOTALDEMAND',
                                                            'DEMANDFORECAST', 'INITIALSUPPLY'],
             table_primary_keys=['SETTLEMENTDATE', 'REGIONID'])
         self.DISPATCHLOAD = InputsBySettlementDate(
@@ -186,10 +273,10 @@ class DBManager:
                                                       'LOWERREGENABLEMENTMAX', 'LOWERREGENABLEMENTMIN'],
             table_primary_keys=['SETTLEMENTDATE', 'DUID'])
         self.DUDETAILSUMMARY = InputsBySettlementDate(
-            table_name='DUDETAILSUMMARY', table_columns=['SETTLEMENTDATE', 'DUID', 'DISPATCHMODE', 'AGCSTATUS',
-                                                      'INITIALMW', 'TOTALCLEARED', 'RAMPDOWNRATE', 'RAMPUPRATE',
-                                                      'AVAILABILITY', 'RAISEREGENABLEMENTMAX', 'RAISEREGENABLEMENTMIN',
-                                                      'LOWERREGENABLEMENTMAX', 'LOWERREGENABLEMENTMIN'],
+            table_name='DUDETAILSUMMARY', table_columns=['DUID', 'START_DATE', 'END_DATE', 'DISPATCHTYPE',
+                                                         'CONNECTIONPOINTID', 'REGIONID', 'STATIONID',
+                                                         'LASTCHANGED', 'TRANSMISSIONLOSSFACTOR',
+                                                         'DISTRIBUTIONLOSSFACTOR'],
             table_primary_keys=['SETTLEMENTDATE', 'DUID'])
         self.DISPATCHCONSTRAINT = InputsBySettlementDate(
             table_name='DISPATCHCONSTRAINT', table_columns=['SETTLEMENTDATE', 'CONSTRAINTID', 'RHS',
--- tests/test_historical_inputs.py
+++ tests/test_historical_inputs.py
@@ -23,3 +23,17 @@ def test_download_to_df():
     assert_frame_equal(output, expected)
     server.terminate()
 
+
+def test_download_to_df_raises_on_missing_data():
+    server = subprocess.Popen('python -m http.server 8080 --bind 127.0.0.1')
+    with pytest.raises(historical_inputs.MissingData):
+        output = historical_inputs.download_to_df(url='http://127.0.0.1:8080/test_files/{table}_{year}{month}01.zip',
+                                                  table_name='table_one', year=2020, month=3)
+    server.terminate()
+
+
+def test_download_to_df_raises_on_url_down():
+    with pytest.raises(historical_inputs.MissingData):
+        output = historical_inputs.download_to_df(url='http://127.0.0.1:8080/test_files/{table}_{year}{month}01.zip',
+                                                  table_name='table_one', year=2020, month=3)
+
