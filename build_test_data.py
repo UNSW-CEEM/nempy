25e91a9c6541d81cd8a37755fd8c9ebe444c0a54
nick-gorman <n.gorman305@gmail.com>
2020-10-02 11:42:52 +1000

    testing on travis.ci

---
+++ build_test_data.py
@@ -0,0 +1,9 @@
+import sqlite3
+from nempy.historical_inputs import mms_db, xml_cache
+
+
+con = sqlite3.connect('market_management_system.db')
+mms_db_manager = mms_db.DBManager(connection=con)
+xml_cache_manager = xml_cache.XMLCacheManager('test_nemde_cache')
+mms_db_manager.populate(start_year=2019, start_month=1, end_year=2019, end_month=1)
+xml_cache_manager.populate(start_year=2019, start_month=1, end_year=2019, end_month=1)
