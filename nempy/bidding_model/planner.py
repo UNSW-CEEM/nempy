1a0dddb474d0de03ffd5e123b9d1cc55acb2c0f6
nick-gorman <40549624+nick-gorman@users.noreply.github.com>
2020-12-21 11:12:25 +1100

    marginal revenue replaces SOS2

--- nempy/bidding_model/planner.py
+++ nempy/bidding_model/planner.py
@@ -10,8 +10,7 @@ class DispatchPlanner:
         self.price_traces_by_node = {}
         self.units = []
         self.unit_node_mapping = {}
-        self.model = Model(solver_name='GUROBI')
-        self.model.max_mip_gap = 0.05
+        self.model = Model(solver_name='GUROBI', sense='MAX')
         self.unit_in_flow_variables = {}
         self.unit_out_flow_variables = {}
         self.unit_storage_input_capacity = {}
@@ -22,38 +21,80 @@ class DispatchPlanner:
         self.unit_storage_initial_mwh = {}
         self.unit_storage_level_variables = {}
         self.market_node_dispatch_variables = {}
-        self.market_node_revenue_variables = {}
-        self.market_node_dispatch_revenue_interpolation_weights = {}
+        self.market_node_net_dispatch_variables = {}
 
     def add_market_node(self, name, price_traces):
         self.market_nodes.append(name)
         self.price_traces_by_node[name] = price_traces
         self.planning_horizon = len(price_traces['interval'])
-        revenue_traces = self._get_revenue_traces(name)
+        positive_rate, positive_dispatch, negative_rate, negative_dispatch = self._marginal_market_trade(name)
         self.market_node_dispatch_variables[name] = {}
-        self.market_node_revenue_variables[name] = {}
-        self.market_node_dispatch_revenue_interpolation_weights[name] = {}
+        self.market_node_net_dispatch_variables[name] = {}
         for i in range(0, self.planning_horizon):
-            dispatch_var_name = "dispatch_{}".format(i)
-            self.market_node_dispatch_variables[name][i] = self.model.add_var(name=dispatch_var_name, lb=-1 * INF, ub=INF)
-            revenue_var_name = "revenue_{}".format(i)
-            self.market_node_revenue_variables[name][i] = self.model.add_var(name=revenue_var_name, lb=-1 * INF, ub=INF)
-            self.market_node_dispatch_revenue_interpolation_weights[name][i] = {}
-            trace_dispatch_levels = [level for level in revenue_traces.columns if level != 'interval']
-            for level in trace_dispatch_levels:
-                interpolation_var_name = "interpolation_{}_{}".format(i, level)
-                self.market_node_dispatch_revenue_interpolation_weights[name][i][level] = self.model.add_var(interpolation_var_name,
-                                                                                           lb=0.0, ub=1.0)
-            self.model += xsum(self.market_node_dispatch_revenue_interpolation_weights[name][i].values()) == 1.0
-            weights = self.market_node_dispatch_revenue_interpolation_weights[name][i]
-            revenue = revenue_traces.iloc[i].to_dict()
-            self.model += xsum([level * weight_var for level, weight_var in weights.items()]) == \
-                          self.market_node_dispatch_variables[name][i]
-            self.model += xsum([revenue[level] * weight_var for level, weight_var in weights.items()]) == \
-                          self.market_node_revenue_variables[name][i]
-            self.model.add_sos([(weight_var, level) for level, weight_var in weights.items()], 2)
+            self.market_node_dispatch_variables[name][i] = {}
+            self.market_node_dispatch_variables[name][i]['positive'] = {}
+            self.market_node_dispatch_variables[name][i]['negative'] = {}
+            self.market_node_net_dispatch_variables[name][i] = {}
+            for dispatch, rate in positive_rate[i].items():
+                dispatch_var_name = "dispatch_{}_{}_positive_{}".format(name, i, dispatch)
+                self.market_node_dispatch_variables[name][i]['positive'][dispatch] = \
+                    self.model.add_var(name=dispatch_var_name, lb=0.0, ub=positive_dispatch[i][dispatch], obj=rate)
+            for dispatch, rate in negative_rate[i].items():
+                dispatch_var_name = "dispatch_{}_{}_negative_{}".format(name, i, dispatch)
+                self.market_node_dispatch_variables[name][i]['negative'][dispatch] = \
+                    self.model.add_var(name=dispatch_var_name, lb=0.0, ub=negative_dispatch[i][dispatch], obj=rate)
 
-        self.model.add_var(var_type=BINARY, obj=0.0)
+            dispatch_var_name = "net_dispatch_{}_{}".format(name, i)
+            self.market_node_net_dispatch_variables[name][i] = \
+                self.model.add_var(name=dispatch_var_name, lb= -1.0 * INF, ub=INF)
+
+            positive_vars = list(self.market_node_dispatch_variables[name][i]['positive'].values())
+            negative_vars = list(self.market_node_dispatch_variables[name][i]['negative'].values())
+            self.model += xsum([-1 * self.market_node_net_dispatch_variables[name][i]] + positive_vars +
+                               [-1 * var for var in negative_vars]) == 0.0
+
+    def _get_revenue_traces(self, name):
+        revenue_traces = self.price_traces_by_node[name]
+        for col in revenue_traces.columns:
+            if col != 'interval':
+                revenue_traces[col] = revenue_traces[col] * (col + 0.00001)
+        return revenue_traces
+
+    def _marginal_market_trade(self, name):
+        revenue_trace = self._get_revenue_traces(name)
+        value_columns = [col for col in revenue_trace.columns if col != 'interval']
+        stacked = pd.melt(revenue_trace, id_vars=['interval'], value_vars=value_columns,
+                          var_name='dispatch', value_name='revenue')
+
+        positive = stacked[stacked['dispatch'] >= 0.0]
+        negative = stacked[stacked['dispatch'] <= 0.0].copy()
+        negative['dispatch'] = negative['dispatch'] * -1.0
+
+        positive = positive.sort_values('dispatch')
+        negative = negative.sort_values('dispatch')
+
+        positive['marginal_revenue'] = positive.groupby('interval', as_index=False)['revenue'].diff()
+        negative['marginal_revenue'] = negative.groupby('interval', as_index=False)['revenue'].diff()
+
+        positive['marginal_dispatch'] = positive.groupby('interval', as_index=False)['dispatch'].diff()
+        negative['marginal_dispatch'] = negative.groupby('interval', as_index=False)['dispatch'].diff()
+
+        positive = positive[positive['dispatch'] != 0.0]
+        negative = negative[negative['dispatch'] != 0.0]
+
+        positive['marginal_rate'] = positive['marginal_revenue'] / positive['marginal_dispatch']
+        negative['marginal_rate'] = negative['marginal_revenue'] / negative['marginal_dispatch']
+
+        positive = positive.set_index(['interval', 'dispatch']).loc[:, ['marginal_rate', 'marginal_dispatch']]
+        negative = negative.set_index(['interval', 'dispatch']).loc[:, ['marginal_rate', 'marginal_dispatch']]
+
+        positive_rate = positive.groupby(level=0).apply(lambda df: df.xs(df.name).marginal_rate.to_dict()).to_dict()
+        positive_dispatch = positive.groupby(level=0).apply(lambda df: df.xs(df.name).marginal_dispatch.to_dict()).to_dict()
+
+        negative_rate = negative.groupby(level=0).apply(lambda df: df.xs(df.name).marginal_rate.to_dict()).to_dict()
+        negative_dispatch = negative.groupby(level=0).apply(lambda df: df.xs(df.name).marginal_dispatch.to_dict()).to_dict()
+
+        return positive_rate, positive_dispatch, negative_rate, negative_dispatch
 
     def add_unit(self, name, market_node_name):
         self.units.append(name)
@@ -112,17 +153,9 @@ class DispatchPlanner:
                 self.model += previous_storage_level - (output_from_storage / output_efficiency) * hours_per_interval + \
                               (input_to_storage * input_efficiency) * hours_per_interval == storage_level
 
-    def _get_revenue_traces(self, name):
-        revenue_traces = self.price_traces_by_node[name]
-        for col in revenue_traces.columns:
-            if col != 'interval':
-                revenue_traces[col] = revenue_traces[col] * (col + 0.00001)
-        return revenue_traces
-
     def optimise(self):
         self._create_constraints_to_balance_grid_nodes()
         self._create_constraints_to_balance_unit_nodes()
-        self._add_objective_function()
         self.model.optimize()
 
     def _create_constraints_to_balance_grid_nodes(self):
@@ -132,7 +165,7 @@ class DispatchPlanner:
                                 unit_name in self.units if node == self.unit_node_mapping[unit_name]]
                 in_flow_vars = [self.unit_in_flow_variables[unit_name][i]["market_to_unit"] for
                                  unit_name in self.units if node == self.unit_node_mapping[unit_name]]
-                self.model += xsum([self.market_node_dispatch_variables[node][i]] + in_flow_vars +
+                self.model += xsum([self.market_node_net_dispatch_variables[node][i]] + in_flow_vars +
                                    [-1 * var for var in out_flow_vars]) == 0.0
 
     def _create_constraints_to_balance_unit_nodes(self):
@@ -142,18 +175,18 @@ class DispatchPlanner:
                 out_flow_vars = [var for var_name, var in self.unit_out_flow_variables[unit][i].items()]
                 self.model += xsum(in_flow_vars + [-1 * var for var in out_flow_vars]) == 0.0
 
-    def _add_objective_function(self):
-        revenue_vars = []
-        for node in self.market_nodes:
-            revenue_vars += [var for name, var in self.market_node_revenue_variables[node].items()]
-        self.model.objective = maximize(xsum(revenue_vars))
+    # def _add_objective_function(self):
+    #     revenue_vars = []
+    #     for node in self.market_nodes:
+    #         revenue_vars += [var for name, var in self.market_node_revenue_variables[node].items()]
+    #     self.model.objective = maximize(xsum(revenue_vars))
 
     def get_dispatch(self):
         trace = self.price_traces_by_node[self.market_nodes[0]].loc[:, ['interval']]
         trace['dispatch'] = \
-            trace['interval'].apply(lambda x: self.model.var_by_name(str("dispatch_{}".format(x))).x, self.model)
-        trace['revenue'] = \
-            trace['interval'].apply(lambda x: self.model.var_by_name(str("revenue_{}".format(x))).x, self.model)
+            trace['interval'].apply(lambda x: self.model.var_by_name(str("net_dispatch_nsw_{}".format(x))).x, self.model)
+        # trace['revenue'] = \
+        #     trace['interval'].apply(lambda x: self.model.var_by_name(str("revenue_{}".format(x))).x, self.model)
         trace['unit_to_market'] = \
             trace['interval'].apply(lambda x: self.model.var_by_name(str("stor_unit_to_market_{}".format(x))).x, self.model)
         trace['market_to_unit'] = \
