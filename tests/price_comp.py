a5d55a5f383556bcea6d387cf8d674c9ebebed37
nick-gorman <n.gorman305@gmail.com>
2024-06-04 11:45:18 +1000

    remove price comp file so tests run

--- tests/price_comp.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-benchmark = pd.read_csv('1000_interval_benchmark.csv')
-outputs = pd.read_csv('latest_1000_interval_run.csv')
-comp = pd.merge(outputs, benchmark, on=['time', 'region', 'service'])
-comp['diff'] = (comp['error_x'].abs() - comp['error_y'].abs())
-comp = comp.sort_values('diff', ascending=False)
-x=1
